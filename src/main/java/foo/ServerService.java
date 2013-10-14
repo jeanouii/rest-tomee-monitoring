b3981046769d7022d56a04e9a25be806da35d525
Jean-Louis MONTEIRO <jeanouii@gmail.com>
2013-10-14 22:14:18 +0200

    Initializing the project

---
+++ src/main/java/foo/ServerService.java
@@ -0,0 +1,11 @@
+package foo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jlmonteiro
+ * Date: 14/10/13
+ * Time: 21:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServerService {
+}
---
+++ src/test/java/foo/ServerServiceTest.java
@@ -0,0 +1,11 @@
+package foo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jlmonteiro
+ * Date: 14/10/13
+ * Time: 21:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class ServerServiceTest {
+}
